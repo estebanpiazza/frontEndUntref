db03ae95855b83d174894fb32e6c34aae1e5f5a4
Esteban Piazza <estebanpiazza51@gmail.com>
2022-07-15 20:21:58 -0300

    crear repaso

---
+++ JS/repaso general/repaso.js
@@ -0,0 +1,54 @@
+// 1) Variables - tipos de datos
+//un espacio en memoria con un nombre
+// let nombre = valor;
+let apellido = "Piazza";
+let edad = 29;
+// string = "palabra"
+// integer-float = enteros o con coma (decimales)
+// boolean = true O false
+// undefined = no esta definido el valor
+// null = valor nulo o vacio
+// objetos = objetos 
+
+// 2) Operadores
+
+// Matematicos
+// Sumar y concatenar : + 
+// numero + numero (suma) - string + string (concatena) - numero+string(concatena)
+// Restar: -
+// Dividir(cociente): /
+// Dividir(resto): %
+// Multiplicar *
+// Potencia **
+
+// Logicos
+// Igualdad
+// == igualdad debil (solo valor)
+// "2" == 2 devuelve true
+// === igualdad estrica (valor y tipo de dato)
+// "2" === 2 devuelve false
+// Desigualdad != debil y !== estricta ( no es igual (o es distinto))
+// Mayor - menor - mayor o igual - menor o igual
+// > - < - >= - <= 
+// devuelven true o false
+console.log(10<11)
+// And y  Or (comparacion)
+// &&  y  || 
+// y   -   o
+//          V    y  V = true
+console.log(1<2 && 2<3)
+//          V   o   V = true
+console.log(1<2 || 2<3)
+//          F    y  V = false
+console.log(1>2 && 2<3)
+//          F    o  V = true
+console.log(1>2 || 2<3)
+
+// 3) Funciones
+
+
+// 4) Estructura if
+
+// 5) Estructura while - do while
+
+// 6) Estructura for
